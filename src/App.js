ca03b72ef80b1bd303ccac48c465da386fcf6dc6
matulibe <57047770+matulibe@users.noreply.github.com>
2023-09-09 01:06:33 -0300

    Comienzo a reordenar dias y actividades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
 import { DndContext, closestCenter } from '@dnd-kit/core';
-import {SortableContext, verticalListSortingStrategy, arrayMove} from '@dnd-kit/sortable';
+import { SortableContext, verticalListSortingStrategy, horizontalListSortingStrategy, arrayMove} from '@dnd-kit/sortable';
 import './App.css';
 import Actividad from './components/Actividad'
+import Dia from './components/Dia';
 
 //Las actividades a crear seran widgets. Por lo tanto ademas de implementarlos, la planilla debera ser
 // un mapa de widgets. 
@@ -15,45 +16,24 @@ import Actividad from './components/Actividad'
  */
 function App() {
 
-  const [actividades, setActividades] = useState([
-    { id: 1, titulo: "Proba" },
-    { id: 2, titulo: "Boxeo" },
-    { id: 3, titulo: "Apollo" },
+  const [dias, setDias] = useState([
+    {
+      id: 1,
+      titulo: 'lunes'
+    }
   ]);
 
-  const [dia, setDias] = useState([]);
-
-  const handleDragEnd = (e) => {
-    /* A tener en cuenta a futuro
-     *
-     *onDragEnd recibe parametros active y over, 
-     *Estos nombres *NO* se pueden modificar
-     *
-     */
-    const {active, over} = e;
-
-    if (!active.id !== over.id) {
-      setActividades((actividades) => {
-        const oldIndex = actividades.findIndex((actividad) => actividad.id === active.id);
-        const newIndex = actividades.findIndex((actividad) => actividad.id === over.id);
-        return arrayMove(actividades, oldIndex, newIndex);
-      });
-    }
-  }
-
   return (
-      <DndContext className="fondo" collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+    <div>
         <h1>Organizador de Horarios</h1>
-
-        <SortableContext items={actividades} strategy={verticalListSortingStrategy}>
-          {
-            actividades.map((actividad) => (
-              <Actividad key={actividad.id} actividad={actividad}  />
-            ))
-          }
-        </SortableContext>
-
-      </DndContext>
+      <SortableContext items={dias} strategy={horizontalListSortingStrategy}>
+        {
+          dias.map((dia) => (
+            <Dia key={dia.id} dia={dia} />
+          ))
+        }
+      </SortableContext>
+    </div>
   );
 }
 
--- src/components/Dia.js
+++ src/components/Dia.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
+import { useState } from 'react';
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable';
+import Actividad from './Actividad'
 
 
 function Dia({ dia }) {
@@ -8,9 +13,42 @@ function Dia({ dia }) {
         transform: CSS.Transform.toString(transform),
         transition
     }
+
+    const [actividades, setActividades] = useState([
+        { id: 1, titulo: "Proba" },
+        { id: 2, titulo: "Boxeo" },
+        { id: 3, titulo: "Apollo" },
+    ]);
+
+    const handleDragEnd = (e) => {
+        /* A tener en cuenta a futuro
+         *
+         *onDragEnd recibe parametros active y over, 
+         *Estos nombres *NO* se pueden modificar
+         *
+         */
+        const { active, over } = e;
+
+        if (!active.id !== over.id) {
+            setActividades((actividades) => {
+                const oldIndex = actividades.findIndex((actividad) => actividad.id === active.id);
+                const newIndex = actividades.findIndex((actividad) => actividad.id === over.id);
+                return arrayMove(actividades, oldIndex, newIndex);
+            });
+        }
+    }
+    
     return (
         <div style={style} ref={setNodeRef} {...attributes} {...listeners}>
-            <h2>{dia.titulo}</h2>
+            <DndContext className="fondo" collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <SortableContext items={actividades} strategy={verticalListSortingStrategy}>
+                    {
+                        actividades.map((actividad) => (
+                            <Actividad key={actividad.id} actividad={actividad} />
+                        ))
+                    }
+                </SortableContext>
+            </DndContext>
         </div>
     )
 }
